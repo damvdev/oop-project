6d4028603690151df9acf0b91723802a65e287cd
damvdev <143938844+damvdev@users.noreply.github.com>
2026-04-13 22:39:25 +0200

    Added full code

---
+++ SchoolDAO/Persistence/Utils/SQLServerUtils.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDAO.Persistence.Utils
+{
+    public class SQLServerUtils
+    {
+        public static string OpenConnection()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            return config.GetConnectionString("DefaultConnection");
+
+        }
+    }
+}
